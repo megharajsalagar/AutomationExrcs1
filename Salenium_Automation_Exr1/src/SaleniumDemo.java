77588d409dfd5eb9e63687a1da031bb5c006762e
megha <megha@DESKTOP-PSRE6VL>
2021-10-04 17:58:02 +0530

    Third commit

--- Salenium_Automation_Exr1/src/SaleniumDemo.java
+++ Salenium_Automation_Exr1/src/SaleniumDemo.java
@@ -1,5 +1,9 @@
+
+/*Author: Megharaj Salagar
+  Date: 01/10/2021
+*/
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,22 +14,29 @@ import java.util.concurrent.TimeUnit;
 
 public class SaleniumDemo {
 
+	/*
+	 * Method Name :exerciseOne Date:01/10/2021
+	 */
 	public static void exerciseOne(WebDriver driver) throws InterruptedException {
 
 		driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[1]/input")).click();
 		Assert.assertTrue(
-				driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[1]/input")).isSelected());
+				driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[1]/input")).isSelected()); //verifying using assertions
 		Thread.sleep(3000L);
 		driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[2]/input")).click();
 		Thread.sleep(3000L);
 		Assert.assertTrue(
-				driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[2]/input")).isSelected());
+				driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[2]/input")).isSelected());//verifying using assertions
 		driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[3]/input")).click();
 		Thread.sleep(3000L);
 		Assert.assertTrue(
-				driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[3]/input")).isSelected());
+				driver.findElement(By.xpath("//div[@id='radio-btn-example']/fieldset/label[3]/input")).isSelected());//verifying using assertions
 
 	}
+	
+	/*
+	 * Method Name :exerciseTwo Date:01/10/2021
+	 */
 
 	public static void exerciseTwo(WebDriver driver) throws InterruptedException {
 
@@ -42,6 +53,10 @@ public class SaleniumDemo {
 		}
 
 	}
+	
+	/*
+	 * Method Name :exerciseThree Date:01/10/2021
+	 */
 
 	public static void exerciseThree(WebDriver driver) throws InterruptedException {
 
@@ -73,6 +88,10 @@ public class SaleniumDemo {
 
 	}
 
+	/*
+	 * Method Name :exerciseFour Date:01/10/2021
+	 */
+	
 	public static void exerciseFour(WebDriver driver) throws InterruptedException {
 		driver.findElement(By.xpath("//input[@id='checkBoxOption1']")).click();// for selecting checkbox1
 		Thread.sleep(500L);
@@ -94,8 +113,11 @@ public class SaleniumDemo {
 		Thread.sleep(500L);
 		Assert.assertFalse(driver.findElement(By.xpath("//input[@id='checkBoxOption3']")).isSelected()); // closing
 																											// driver
-
 	}
+	
+	/*
+	 * Method Name :exerciseFourPointOne Date:01/10/2021
+	 */
 
 	public static void exerciseFourPointOne(WebDriver driver) throws InterruptedException {
 
@@ -109,6 +131,10 @@ public class SaleniumDemo {
 
 	}
 
+	/*
+	 * Method Name :exerciseFourPointTwo Date:04/10/2021
+	 */
+	
 	public static void exerciseFourPointTwo(WebDriver driver) throws InterruptedException {
 		driver.findElement(By.xpath("//input[@id='checkBoxOption1']")).click();
 		driver.findElement(By.xpath("//input[@id='checkBoxOption2']")).click();
@@ -120,10 +146,10 @@ public class SaleniumDemo {
 
 	}
 	
-	/**
-	 * @param driver
-	 * @throws InterruptedException
+	/*
+	 * Method Name :exerciseFive Date:04/10/2021
 	 */
+	
 	public static void exerciseFive(WebDriver driver)  throws InterruptedException {
 		
 		  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -141,15 +167,17 @@ public class SaleniumDemo {
 		 * handler=driver.getWindowHandle(); Thread.sleep(3000); //driver.close();
 		 * driver.switchTo().window(handler).close(); Thread.sleep(2000);
 		 */
-		  
-		
 	}
+	
+	/*
+	 * Method Name :exerciseSix Date:04/10/2021
+	 */
 	
 	public static void exerciseSix(WebDriver driver) throws InterruptedException{
 		  driver.findElement(By.xpath("//*[@id='opentab']")).click(); 
 		  Set<String>Windows = driver.getWindowHandles();
 		  Iterator<String> it =Windows.iterator(); 
-		  String parentid=it.next();
+		  String parentid =it.next();
 		  Thread.sleep(3000); 
 		  String child=it.next();
 		  driver.switchTo().window(child);
@@ -158,7 +186,27 @@ public class SaleniumDemo {
 		
 	}
 	
+	/*
+	 * Method Name :exerciseSeven Date:04/10/2021
+	 */
+	
+	public static void exerciseSeven(WebDriver driver) throws InterruptedException {
+		driver.findElement(By.xpath("//*[@id='name']")).sendKeys("John");
+		driver.findElement(By.xpath("//*[@id='alertbtn']")).click();
+		Thread.sleep(3000);
+		driver.close();
+	}
 
+
+	/*
+	 * Method Name :exerciseEight Date:04/10/2021
+	 */
+	
+	public static void exerciseEight(WebDriver driver) throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,1000)");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:\\Qualitest\\driver\\chromedriver.exe");
@@ -174,15 +222,15 @@ public class SaleniumDemo {
 			SaleniumDemo.exerciseFourPointTwo(driver);
 			SaleniumDemo.exerciseFive(driver);
 			SaleniumDemo.exerciseSix(driver);
-
+			SaleniumDemo.exerciseSeven(driver);
+			SaleniumDemo.exerciseEight(driver);
 			driver.close();
 		}
 
 		catch (InterruptedException e) {
 			System.out.println(e);
 		}
-
-		// driver.close();
+		
 	}
 
 }
